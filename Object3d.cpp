f85566aad6e4e62e10e2cf5264ac994f539033e9
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-07 14:35:04 +0900

    ファイルの中身を読み込む処理

--- Object3d.cpp
+++ Object3d.cpp
@@ -421,6 +421,21 @@ void Object3d::CreateModel()
 		assert(0);
 	}
 
+	vector<XMFLOAT3>positions;//頂点座標
+	vector<XMFLOAT3>normals;//法線ベクトル
+	vector<XMFLOAT2>texcoords;//テクスチャUV
+	//1行ずつ読み込む
+	string line;
+	while (getline(file, line)) {
+		//1行分の文字列をストリームに変換して解析しやすくする
+		std::istringstream line_stream(line);
+
+		//半角スペース区切りで行の先頭文字列を取得
+		string key;
+		getline(line_stream,key,' ');
+
+	}
+
 	HRESULT result = S_FALSE;
 
 	std::vector<VertexPosNormalUv> realVertices;
