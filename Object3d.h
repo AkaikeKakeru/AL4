256c2b8fa08a67a0f1325d5c550d16db6931abe6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-10-14 15:24:16 +0900

    vertexCountを、CG3の時のに戻す

--- Object3d.h
+++ Object3d.h
@@ -41,9 +41,9 @@ private: // 定数
 	static const float radius;				// 底面の半径
 	static const float prizmHeight;			// 柱の高さ
 	static const int planeCount = division * 2 + division * 2;		// 面の数
-	//static const int vertexCount = planeCount * 3;		// 頂点数
+	static const int vertexCount = planeCount * 3;		// 頂点数
 	
-	static const int vertexCount = 4;		// 頂点数
+	//static const int vertexCount = 3;		// 頂点数
 	static const int indexCount = 3 * 2;		// インデックス数
 
 public: // 静的メンバ関数
@@ -152,9 +152,9 @@ private: // 静的メンバ変数
 	//static unsigned short indices[planeCount * 3];
 
 	// 頂点データ配列
-	static VertexPosNormalUv vertices[vertexCount];
+	static std::vector<VertexPosNormalUv> vertices;
 	// 頂点インデックス配列
-	static unsigned short indices[indexCount];
+	static std::vector<unsigned short> indices;
 
 private:// 静的メンバ関数
 	/// <summary>
