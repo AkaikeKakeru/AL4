4d878355c7b421f150b9c431631fe5dbbcc359b6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-18 14:18:28 +0900

    定数バッファを増やす

--- Object3d.h
+++ Object3d.h
@@ -241,7 +241,8 @@ public: // メンバ関数
 	void SetPosition(const XMFLOAT3& position) { this->position = position; }
 
 private: // メンバ変数
-	ComPtr<ID3D12Resource> constBuff; // 定数バッファ
+	ComPtr<ID3D12Resource> constBuffB0; // 定数バッファ
+	ComPtr<ID3D12Resource> constBuffB2; // 定数バッファ
 	// 色
 	XMFLOAT4 color = { 1,1,1,1 };
 	// ローカルスケール
