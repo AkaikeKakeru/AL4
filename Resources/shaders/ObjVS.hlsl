5933f0932e0b0472b78b2b5cbcb68c6d70449fd5
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-18 13:58:38 +0900

    ObjVS.hlslの中身を記述

--- Resources/shaders/ObjVS.hlsl
+++ Resources/shaders/ObjVS.hlsl
@@ -1,6 +1,11 @@
 #include "Obj.hlsli"
 
-float4 main( float4 pos : POSITION ) : SV_POSITION
-{
-	return pos;
+VSOutput main( float4 pos : POSITION, float3 normal:NORMAL, float2 uv:TEXCOORD ){
+	VSOutput output; //ピクセルシェーダーに渡す値
+	
+	output.svpos = mul(mat, pos);
+	output.normal = normal;
+	output.uv = uv;
+
+	return output;
 }
