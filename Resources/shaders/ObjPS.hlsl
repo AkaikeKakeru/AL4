f19a88525635defaadd9fa66fb66d33082ab1e61
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-11-18 14:10:05 +0900

    誤字修正

--- Resources/shaders/ObjPS.hlsl
+++ Resources/shaders/ObjPS.hlsl
@@ -4,7 +4,7 @@ Texture2D<float4>tex : register(t0); //0番スロットに設定されたテクスチャ
 SamplerState smp : register(s0); //0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET{
-	float 3 light = normalize(float3(1,-1,1)); // 右下奥 向きのライト
+	float3 light = normalize(float3(1,-1,1)); // 右下奥 向きのライト
 	float light_diffuse = saturate(dot(-light, input.normal));
 	float3 shade_color;
 
@@ -12,5 +12,5 @@ float4 main(VSOutput input) : SV_TARGET{
 	shade_color += m_diffuse * light_diffuse; //ディフューズ項
 	
 	float4 texcolor = tex.Sample(smp, input.uv);
-	return float4(tex_color.rgb * shade_color,texcolor.a * m_alpha);
+	return float4(texcolor.rgb * shade_color,texcolor.a * m_alpha);
 }
