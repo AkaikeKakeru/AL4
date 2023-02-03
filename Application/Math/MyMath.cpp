6615ed0958f9da858846087ab92596eab940893a
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-02-04 01:56:34 +0900

    平方根を求める関数を用意

--- Application/Math/MyMath.cpp
+++ Application/Math/MyMath.cpp
@@ -13,3 +13,20 @@ float AbsoluteValue(float f) {
 	f = (f * f) / f;
 	return f;
 }
+
+float SquareRoot(float f) {
+	const float EPSILON = 0.00001f;
+
+	float x = 1, y = 0;
+
+	//ニュートン法を用いる
+	while (true) {
+		x = x - (x * x - f) / (2 * x);
+		y = x * x - f;
+		if ((y <= EPSILON) && (y >= -EPSILON)){
+			break;
+		}
+	}
+
+	return x;
+}
--- Application/Math/MyMath.h
+++ Application/Math/MyMath.h
@@ -4,3 +4,5 @@
 float ConvertToRadian(float degree);
 //絶対値
 float AbsoluteValue(float f);
+//平方根
+float SquareRoot(float f);
