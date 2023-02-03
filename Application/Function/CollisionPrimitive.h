2f87efd03dca80f2b5a3f3a53e4140d42fe08eb6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-02-03 22:27:42 +0900

    三角形のデータ型作成

--- Application/Function/CollisionPrimitive.h
+++ Application/Function/CollisionPrimitive.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Vector3.h"
 
+//球
 struct Sphere {
 	//中心
 	Vector3 center_ = { 0,0,0 };
@@ -9,10 +10,22 @@ struct Sphere {
 	float radius_ = 1.0f;
 };
 
+//平面
 struct Plane {
 	//法線
 	Vector3 normal_ = { 0,1,0 };
 
 	//原点からの距離
 	float distance_ = 0.0f;
+};
+
+//法線付き三角形(時計回りが表面)
+class Triangle{
+public:
+	//頂点座標3つ
+	Vector3 p0_;
+	Vector3 p1_;
+	Vector3 p2_;
+	//法線ベクトル
+	Vector3 normal_;
 };
