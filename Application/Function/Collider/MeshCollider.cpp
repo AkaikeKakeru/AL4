7f6a9f4a85992b032df4a4e178bc6deabd5e231c
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-02-06 22:34:30 +0900

    三角形の配列を構築を定義

---
+++ Application/Function/Collider/MeshCollider.cpp
@@ -0,0 +1,47 @@
+#include "MeshCollider.h"
+#include "Collision.h"
+#include <iostream>
+
+void MeshCollider::ConstructTriangle(Model* model) {
+	//三角形リストをクリア
+	triangles_.clear();
+
+	//モデルの持つメッシュリストを取得
+	const std::vector<Mesh*>& meshes = model->GetMeshes();
+
+	//現在のメッシュの開始三角形番号を入れておく変数(0で初期化)
+	int start = 0;
+	//全メッシュについて順に処理する
+	std::vector<Mesh*>::const_iterator it = meshes.cbegin();
+	for (; it != meshes.cend(); ++it) {
+		Mesh* mesh = *it;
+
+		const std::vector<Mesh::VertexPosNormalUv>& vertices = mesh->GetVertices();
+		const std::vector<unsigned short>& indices = mesh->GetIndices();
+
+		//インデックスは、三角形の数×3個あるので、
+		//そこからメッシュ内の三角形の数を逆算する
+		size_t triangleNum = indices.size() / 3;
+	
+		//現在のメッシュの三角形の数だけ、三角形リストにスペースを追加する
+		triangles_.resize(triangles_.size() + triangleNum);
+
+		//全三角形について順に処理する
+		for (int i = 0; i < triangleNum; i++) {
+			Triangle& tri = triangles_[start + i];
+			int idx0 = indices[i * 3 + 0];
+			int idx1 = indices[i * 3 + 1];
+			int idx2 = indices[i * 3 + 2];
+
+			//三角形の3頂点の座標を代入
+			tri.p0_ = { vertices[idx0].pos.x,vertices[idx0].pos.y,vertices[idx0].pos.z};
+			tri.p1_ = { vertices[idx1].pos.x,vertices[idx1].pos.y,vertices[idx1].pos.z};
+			tri.p2_ = { vertices[idx2].pos.x,vertices[idx2].pos.y,vertices[idx2].pos.z};
+		
+			//3頂点から法線を計算
+			tri.ComputeNormal();
+		}
+		//次のメッシュは、今までの三角形番号の次から使う
+		start += (int)triangleNum;
+	}
+}
