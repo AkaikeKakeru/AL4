1b8e7351fda2ebe33d03affbe97c934987a3a534
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-02-06 22:32:21 +0900

    メッシュコライダーを宣言

---
+++ Application/Function/Collider/MeshCollider.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "BaseCollider.h"
+#include "CollisionPrimitive.h"
+
+class MeshCollider :
+public BaseCollider{
+public:
+	MeshCollider() {
+		//メッシュ形状をセット
+		shapeType_ = COLLISIONSHAPE_MESH;
+	}
+
+	//三角形の配列を構築する
+	void ConstructTriangle(Model* model);
+
+	//更新
+	void Update()override;
+
+	//球との衝突判定
+	bool CheckCollisionSphere(const Sphere& sphere,
+		Vector3* inter = nullptr);
+
+	//レイとの衝突判定
+	bool CheckCollisionRay(const Ray& ray, float* distance = nullptr,
+		Vector3* inter = nullptr);
+
+private:
+	std::vector<Triangle> triangles_;
+	//ワールド行列の逆行列
+	Matrix4 invMatWorld_;
+};
